b1679bdb2f8dc523e1df13cb1ea4b2bc9eb43aec
wuhong <wuh131420@foxmail.com>
2018-08-15 19:19:45 +0800

    2018-08-15-diff[添加]
    添加dialogvisible变量,和open/hide突变

--- src/vuex/index.js
+++ src/vuex/index.js
@@ -1,9 +1,15 @@
 export const state = {
-  
+  dialogVisible:false,
+  dialogForm:{}
 }
 
 export const mutations = {
-
+  handleOpenDialog(state,params){
+    state.dialogVisible = true
+  },
+  handlehideDialog(state,params){
+    state.dialogVisible = false
+  },
 }
 
 export const actions = {
