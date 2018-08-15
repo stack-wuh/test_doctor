49eb963b4133fdcccdfee5f502816cea3dc1a645
wuhong <wuh131420@foxmail.com>
2018-08-15 19:15:44 +0800

    2018-08-15-add[提交表格内按钮点击事件]

---
+++ src/vuex/tables/click.js
@@ -0,0 +1,32 @@
+
+/**
+ * 表格内按钮事件
+ * 抽取页面组件按钮
+ * 打开dialog对话框
+ * dialog对话框的visible由vuex控制
+ * 事件传递mutations
+ * params
+ * 
+ */
+export const openDialog = params => {
+   window.$store.commit('handleOpenDialog') 
+}
+
+/**
+ * 表格内按钮事件
+ * 跳转到指定页面事件
+ * params
+ * 
+ */
+export const jump2Other = params => {
+  let {menu , subMenu ,child , path} = params
+  let rootPath = '' , other = ''
+  switch(child || subMenu){
+    case '角色权限' : rootPath = '/system/limit/setting'
+      break ;
+    case '自动回复配置' : rootPath = '/system/store/reply'
+      break ;
+    default : rootPath = '/index'
+  }
+  window.$route.push({path:'/mid/container',query:Object.assign(params,{path:rootPath})})
+}
