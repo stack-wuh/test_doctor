6a050de9257ae1f59f9286e652f27e6fc44b2b7f
wuhong <wuh131420@foxmail.com>
2018-08-16 18:55:50 +0800

    2018-08-16-add[添加意见反馈组件]

---
+++ src/components/serveManage/child/replyPub.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="wrapper" >
+    
+  </section>
+</template>
+
+<script>
+export default {
+  name: '',
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style scoped lang='scss' >
+
+</style>
