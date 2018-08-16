d2734aea356824343980c87077c7765481469bdb
wuhong <wuh131420@foxmail.com>
2018-08-16 18:54:12 +0800

    2018-08-16-add[添加抽奖发布组件]

---
+++ src/components/cuoponManage/child/lottoPub.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="wrapper">
+    <section class="content">
+      <Search />
+      <my-table header="true" :list="[{}]" >
+        <span slot="title">{{$route.query.child}}</span>
+      </my-table>
+    </section>
+  </section>
+</template>
+
+<script>
+import Search from '@/components/common/search'
+import MyTable from '@/components/common/myTable'
+import MyButton from '@/components/common/myButton'
+export default {
+  name: 'lottoPub',
+  components:{
+    Search ,
+    MyTable , 
+    MyButton ,
+  },
+  data () {
+    return {
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style scoped lang='scss' >
+
+</style>
