b744ec1359dd1710da4cf251cc995a35ba8cdd53
wuhong <wuh131420@foxmail.com>
2018-08-17 18:12:04 +0800

    2018-08-17-diff[新增][新增工具函数]

--- src/main.js
+++ src/main.js
@@ -7,10 +7,12 @@ import '../src/assets/style/base.scss'
 import '../src/assets/style/el-base.scss'
 import '../src/assets/style/font/iconfont.css'
 import $http from '../src/utils/axios'
+import {_g} from '../src/utils/global'
+
 
 window.$route = router
 window.$store = store
-
+window._g = _g
 Vue.prototype.$http = $http
 Vue.config.productionTip = false
 
