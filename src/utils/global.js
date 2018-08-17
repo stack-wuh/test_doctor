bb41b242a9eaabd96865af0d6d20711b8acf6229
wuhong <wuh131420@foxmail.com>
2018-08-17 18:15:11 +0800

    2018-08-17-diff[新增][添加工具函数]

--- src/utils/global.js
+++ src/utils/global.js
@@ -1,4 +1,4 @@
-const  _g = {
+export const  _g = {
   toastMsg({type = 'error',msg:message = '服务连接错误,请稍后重试!'} = {}){
     window.$message({
       type ,
@@ -6,5 +6,3 @@ const  _g = {
     })
   }
 }
-
-export default _g
