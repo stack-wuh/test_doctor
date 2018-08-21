fd859a188803cc693a689c5b2c81d6bbd3037e0e
wuhong <wuh131420@foxmail.com>
2018-08-21 18:56:26 +0800

    2018-08-21-diff[新增][新增mapState方法]

--- src/components/common/myButton.vue
+++ src/components/common/myButton.vue
@@ -2,7 +2,7 @@
   <section class="wrapper">
     <section class="content">
       <div class="btn-list" v-for="(item,index) in getBtnList" :key="index">
-        <el-button v-if="item.type == '' || item.type =='button'" v-on:click="item.click(params)" >{{item.text}}</el-button>
+        <el-button v-if="item.type == '' || item.type =='button'" v-on:click="item.click({params , choose})" >{{item.text}}</el-button>
         <el-select v-if="item.type == 'select'" :placeholder="item.text" :size="item.size" :style="item.style" v-model="item.value" >
           <el-option label="aa" value="aa"></el-option>
         </el-select>
@@ -12,7 +12,7 @@
 </template>
 
 <script>
-
+import {mapState} from 'vuex'
 export default {
   name: 'myButton',
   props:['name'],
@@ -23,13 +23,14 @@ export default {
   },
   
   computed:{
+    ...mapState({
+      'choose':'choose'
+    }),
     getBtnList(){
       return this.$store.getters.getBtnLists({name:this.name || this.$route.query.child || this.$route.query.subMenu})
     },
-
   },
   methods: {
-
 
   },
   created(){
