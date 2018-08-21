36368102fb9b0be4219698fa0e8abc8c6a3d58dd
wuhong <wuh131420@foxmail.com>
2018-08-21 15:50:58 +0800

    2018-08-21-diff[更新][完成登陆登出]

--- src/vuex/modules/sign.js
+++ src/vuex/modules/sign.js
@@ -17,15 +17,25 @@ const actions = {
     let length = Object.values(rootState.form).length > 0 ? true : false
     let result = length && Object.values(rootState.form).every(item => item)
     if(result){
-      $http.post('employee/Login.do',rootState.form)
+      $http.post('employee/Login.do',rootState.form ,res => {
+        if(res.status == 0){
+          setTimeout(()=>{
+            window.$route.push({name:'index'})
+          },1000)
+        }
+      } )
     }else{
       window._g.toastMsg({type:'error',msg:'请编辑必填项后提交'})
     }
   },
 
   signOut(){
-    $http.post('employee/doLogout.do',(res)=>{
-      console.log(res)
+    $http.post('employee/doLogout.do', {} ,res => {
+      if(res.status == 0){
+       setTimeout(()=>{
+          window.$route.push({name:'signin'})
+       },1000)
+      }
     })
   }
 }
