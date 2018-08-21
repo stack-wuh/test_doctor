eea1389347a66b9e1fc37a85647639b7e2bcc9e1
wuhong <wuh131420@foxmail.com>
2018-08-21 18:58:39 +0800

    2018-08-21-diff[新增][添加发布交互]

--- src/components/systemManage/child/reply.vue
+++ src/components/systemManage/child/reply.vue
@@ -1,15 +1,26 @@
 <template>
   <section class="wrapper">
       <section class="content">
-          <el-form :model="form" ref="myForm" label-width="120px" >
-            <el-form-item label="关键词" >
-              <el-input v-model="form.keyWord" placeholder="请编辑关键词(客服,咨询电话)"></el-input>
-            </el-form-item>
-            <el-form-item label="回复类型">
-              <el-select placeholder="请选择" v-model="form.type" >
-                <el-option label="aaa" value="aaa"></el-option>
-              </el-select>
-            </el-form-item>
+          <el-form class="my-form"  :model="form" ref="myForm" label-width="120px" >
+            <section class="inline-box">
+              <el-form-item label="关键词" >
+                <el-input v-model="form.keyword" placeholder="请编辑关键词(客服,咨询电话)"></el-input>
+              </el-form-item>
+              <el-form-item label="回复类型">
+                <el-select placeholder="请选择回复类型" v-model="form.type">
+                  <el-option label="文本" value="0"></el-option>
+                  <el-option label="图文" value="1"></el-option>
+                </el-select>
+              </el-form-item>
+            </section>
+            <section class="inline-box">
+              <el-form-item label="状态">
+                <el-select v-model="form.state" placeholder="请选择状态">
+                  <el-option label="禁用" value="0"></el-option>
+                  <el-option label="启用" value="1"></el-option>
+                </el-select>
+              </el-form-item>
+            </section>
             <el-form-item label="回复图片">
               <el-upload
                 class="avatar-uploader"
@@ -20,10 +31,10 @@
               </el-upload>
             </el-form-item>
             <el-form-item label="回复内容" >
-              <el-input placeholder="请编辑回复内容" v-model="form.content" ></el-input>
+              <el-input placeholder="请编辑回复内容" type="textarea" :rows="3" v-model="form.content" ></el-input>
             </el-form-item>
           </el-form>
-          <MyBottom type="button" />
+          <MyBottom type="button" :formData="form" />
       </section>
   </section>
 </template>
@@ -43,10 +54,11 @@ export default {
       uploadUrl:'',
       imageUrl:'',
       form:{
-        keyWord:'',
+        keyword:'',
         type:'',
-        img:'',
+        picture:'http://a4.att.hudong.com/50/35/01300000110309121921352591438.gif',
         content:'',
+        state:'',
       }
     }
   },
@@ -56,12 +68,22 @@ export default {
 </script>
 
 <style scoped lang='scss' >
+@import '../../../assets/style/mixin.scss';
 .wrapper{
   height: 100%;
   .content{
     height: 100%;
     padding: 20px 0;
     background-color: #fff;
+    .inline-box{
+      @include flex-box(row,nowrap,space-between,center);
+      .el-form-item{
+        width: 50%;
+      }
+    }
+    .my-form{
+      width: 80%;
+    }
   }
 }
 </style>
