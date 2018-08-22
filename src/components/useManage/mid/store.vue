66b3f2d239a00e447806bed2348661d814641e56
wuhong <wuh131420@foxmail.com>
2018-08-22 17:48:39 +0800

    2018-08-22-diff[更新][获取用品模块入口数据]

--- src/components/useManage/mid/store.vue
+++ src/components/useManage/mid/store.vue
@@ -1,13 +1,14 @@
 <template>
   <section class="father">
       <section class="content">
-        <Search  />
-        <my-table v-if="isShow" :list="[{}]" header="true" >
+        <Search @inputChange="getList({path:changePath})"  />
+        <my-table v-if="isShow" :list="list" header="true" >
           <span slot="title">{{$route.query.child || $route.query.subMenu}}列表</span>
           <div slot="right">
             <my-button />
           </div>
         </my-table>
+        <my-bottom :total="total" :currentPage="currentPage" />
       </section>
   </section>
 </template>
@@ -19,12 +20,16 @@
 import MyTable from '@/components/common/myTable'
 import Search from '@/components/common/search'
 import MyButton from '@/components/common/myButton'
+import MyBottom from '@/components/common/bottom'
+
+import {mapActions ,mapState ,mapGetters} from 'vuex'
 export default {
   name: 'store',
   components:{
     MyTable , 
     Search ,
     MyButton ,
+    MyBottom ,
   },
   data () {
     return {
@@ -32,19 +37,38 @@ export default {
     }
   },
   computed:{
+    ...mapState({
+      'testlist' : state => state.Use.data,
+      'total' : state => state.Use.total,
+      'currentPage' : state => state.Use.currentPage
+    }),
+    ...mapGetters({
+      'formatUseData':'formatUseData'
+    }),
     changePath(){
       return this.$route.query.child || this.$route.query.subMenu
+    },
+    list(){
+      return this.formatUseData({path:this.changePath})
     }
   },
   watch:{
     changePath(){
       this.isShow = false
+      this.getList({path:this.changePath})
       setTimeout(()=>{
         this.isShow = true
       })
     }
   },
-  methods: {}
+  methods: {
+    ...mapActions({
+      'getList' : 'getUseStore'
+    })
+  },
+  created(){
+    this.getList({path:this.changePath})
+  }
 }
 </script>
 
