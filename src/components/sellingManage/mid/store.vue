276f272e53701a0523f14c71039b7a5906ef3e15
wuhong <wuh131420@foxmail.com>
2018-08-24 18:08:54 +0800

    2018-08-24-add[新增][进销存模块入口]

---
+++ src/components/sellingManage/mid/store.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="father">
+      <section class="content">
+        <Search />
+        <my-table v-if="isShow" :list="[{}]" header="true" :params="changePath" >
+          <span slot="title">{{changePath}}列表</span>
+          <div slot="right">
+            <my-button></my-button>
+          </div>
+        </my-table>
+      </section>
+  </section>
+</template>
+
+<script>
+import Search from '@/components/common/search'
+import MyTable from '@/components/common/myTable'
+import MyButton from '@/components/common/myButton'
+export default {
+  name: 'list',
+  components:{
+    Search ,
+    MyTable ,
+    MyButton ,
+  },
+  data () {
+    return {
+      isShow:true,
+    }
+  },
+  computed:{
+    changePath(){
+      return this.$route.query.child ||  this.$route.query.subMenu
+    }
+  },
+  watch:{
+    changePath(){
+      this.isShow = false
+      setTimeout(()=>{
+        this.isShow = true
+      })
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style scoped lang='scss' >
+
+</style>
