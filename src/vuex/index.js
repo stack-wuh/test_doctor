55e8abfee322f7d3acd19d7f537e02c4a6b517ca
wuhong <wuh131420@foxmail.com>
2018-08-27 19:16:26 +0800

    2018-08-27-diff[更新][计算各个select下拉框的数据]

--- src/vuex/index.js
+++ src/vuex/index.js
@@ -4,11 +4,16 @@ export const state = {
   dialogForm:{},
   isShowLoading:false,
   form:{},
-  search:{} , 
+  search:{
+    currPageNo:1,
+  } , 
   tableRow:{},
   choose:'',
   departmentList:[],
-  roleList:[]
+  roleList:[],
+  couponList:[],
+  pushList:[],
+  carTypeList:[],
 }
 
 export const mutations = {
@@ -48,7 +53,43 @@ export const mutations = {
    */
   setRoleList(state ,{params} ={}){
     state.roleList = params && params
-  }
+  },
+  /**
+   * 清空表单
+   * 切换路由时
+   * 清空form编辑表单内容
+   */
+  handleClear(state){
+    state.form = {}
+  },
+  /**
+   * 设置表单
+   * computed 计算属性
+   * 重置form 表单内容
+   */
+  setFormData(state, value){
+    state.form = value
+  },
+  /**
+   * 保存卡券列表 select
+   */
+  setCouponList(state,{params} = {}){
+    state.couponList = params && params
+  },
+  /**
+   * 设置关注消息推送内容
+   * @param {*} state 
+   * @param {*} param1 
+   */
+  setPushList(state, {params} = {}){
+    state.pushList = params && params
+  },
+  /**
+   * 保存汽车精品类别列表
+   */
+  setCarTypeList(state,{params} = {}){
+    state.carTypeList = params && params
+  } 
 }
 
 export const actions = {
@@ -86,6 +127,39 @@ export const actions = {
     $http.post('roleBackend/getRoleDrop.do' ,{} ,res => {
       commit('setRoleList' ,{params:res.data})
     })
+  },
+  /**
+   * 清空form表单
+   */
+  clearForm({commit}){
+    commit('handleClear')
+  },
+  /**
+   * select  获取卡券列表
+   */
+  getCouponList({commit}){
+    $http.post('platform/getCouponDrop.do', {}, res => {
+      commit('setCouponList', {params: res.data})
+    })
+  },
+  /**
+   * 消息关注后推送
+   * @param {*} param0 
+   * @param {*} param1 
+   */
+  getPushList({commit}, {pushType} = {}){
+    $http.post('platform/getPushContent.do', {pushType}, res => {
+      commit('setPushList', {params:res.data})
+    })
+  },
+
+  /**
+   * 汽车精品管理 -- 精品类别列表 -- select
+   */
+  getCarTypeList({commit}){
+    $http.post('quality/qualityTypeDrop.do', {}, res => {
+      commit('setCarTypeList', {params: res.data})
+    })
   }
 }
 
@@ -95,5 +169,14 @@ export const getters = {
   },
   formatRoleList(state){
     return state.roleList
+  },
+  formatCouponList(state){
+    return state.couponList
+  },
+  formatPushList(state){
+    return state.pushList
+  },
+  formatCarTypeList(state){
+    return state.carTypeList
   }
 }
