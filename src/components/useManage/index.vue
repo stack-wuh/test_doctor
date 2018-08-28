bb02bdf635da3a9cd811b84b0977451fdad696ae
wuhong <wuh131420@foxmail.com>
2018-08-28 11:47:48 +0800

    2018-08-28-diff[更新][会员模块主路由样式]

--- src/components/useManage/index.vue
+++ src/components/useManage/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <section class="father">
-    <header class="header compnent_header">
+  <!-- <section class="father">
+     <header class="header compnent_header">
        <span>{{$route.query.menu}}</span>
        <span v-if="$route.query.subMenu">></span>
        <span>{{$route.query.subMenu}}</span>
@@ -10,31 +10,46 @@
      <section class="content">
        <router-view></router-view>
      </section>
-  </section>
+  </section> -->
 </template>
 
 <script>
 export default {
   name: 'index',
-
   data () {
     return {
+
     }
   },
 
-  methods: {}
+  methods: {},
+  created(){
+   
+  }
 }
 </script>
 
 <style scoped lang='scss' >
-.father{
-  height: 100%;
-  header.header{
-    height: 30px;
-    color: #999;
-  }
-  >.content{
-    height: calc(100% - 30px);
-  }
-}
+// @import '../../assets/style/mixin.scss';
+// .father{
+//   @include flex-box(column,nowrap,flex-start,flex-start);
+//   height:100%;
+//   overflow-y: scroll;
+//   &::-webkit-scrollbar{
+//     width:5px;
+//     height:1px;
+//   }
+//   &::-webkit-scrollbar-thumb{
+//     box-shadow: 0 0 5px inset rgba(0,0,0,.3);
+//     border-radius:4px;
+//   }
+//   header.header{
+//     height:30px;
+//     color: #999;
+//   }
+//   section.content{
+//     flex:1;
+//     width:100%;
+//   }
+// }
 </style>
