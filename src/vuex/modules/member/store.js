f5b28417e92d165f8fff359f8174579acca03735
wuhong <wuh131420@foxmail.com>
2018-09-10 10:25:32 +0800

    2018-09-10-fixed[修复了一些提交的字段]

--- src/vuex/modules/member/store.js
+++ src/vuex/modules/member/store.js
@@ -12,11 +12,53 @@ const actions = {
   getMemberStore({commit, rootState}, {path, search, currPageNo = 1} = {}){
     let _url = ''
     switch(path){
-      case '会员列表' : _url = 'vipList/init.do', search = {...rootState.search, ...search, currPageNo}
+      case '会员列表' : _url = 'vipList/init.do', search = {
+        realName:'',
+        phone:'',
+        grade:'',
+        sex:'',
+        state:'',
+        userName:'',
+        plateNum:'',
+        carModel:'',
+        startCreateTime:'',
+        endCreateTime:'',
+        minIntegral:'',
+        maxIntegral:''
+        , ...rootState.search, ...search, currPageNo}
         break;
     }
     $http.post(_url, search, res => {
       commit('setMemberStore',{params: res.data})
+    })
+  },
+
+  /**
+   * 会员管理模块
+   * 会员列表 
+   * 编辑/新增会员信息
+   */
+  memberInfoPubAndPut({dispatch}, {path, form, form:{id}} = {}){
+    return new Promise((resolve, reject) => {
+      $http.post('vipList/insertOrUpdate.do', form, res => {
+        return resolve(res)
+      })
+    })
+  },
+  /**
+   * 会员管理模块
+   * 会员列表
+   * 导入数据
+   */
+  memberInfoImport({commit,dispatch}, {path, form}){
+    form.fileName = form.fileName.tolocalString()
+    return new Promise((resolve,reject) => {
+      $http.post('vipList/imports.do', form, res => {
+        setTimeout(()=>{
+          dispatch('handleDialogWithImport',{visible:false})
+          return resolve()
+        },1000)
+      })
     })
   }
 }
