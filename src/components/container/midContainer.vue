3b0b000c3e757ab503abb37dc27a2b6e8301e3c7
wuhong <wuh131420@foxmail.com>
2018-09-10 19:05:09 +0800

    2018-09-10-diff[测试代码]

--- src/components/container/midContainer.vue
+++ src/components/container/midContainer.vue
@@ -18,6 +18,7 @@ export default {
   },
   watch:{
     '$route'(to,from){
+      console.log('aaa')
       if(to.query.path){
         this.$router.push({path:to.query.path,query:{menu:to.query.menu , subMenu:to.query.subMenu , child:to.query.child , data:to.query.data}})
       }
