140bf7dfc38c68cb1a9ca474ea6e8b4ae261c2a2
wuhong <wuh131420@foxmail.com>
2018-09-10 10:21:53 +0800

    2018-09-10-add[新增了一个button组件]

---
+++ src/components/common/subButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="wrapper">
+      <section class="btn-area">
+        <el-button type="danger">取消</el-button>
+        <el-button @click="handleSubmit" type="primary">确定</el-button>
+      </section>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'subButton',
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+    handleSubmit(){
+      this.$emit('handleSubmit', {submit:true })
+    }
+  }
+}
+</script>
+
+<style scoped lang='scss' >
+.wrapper{
+  margin:15px 0;
+  .btn-area{
+    text-align:center;    
+  }
+}
+</style>
