14a68853e4c8675df6b41f37ce6f5ddc9cbdb196
wuhong <wuh131420@foxmail.com>
2018-09-10 10:21:31 +0800

    2018-09-10-add[新增了一个detail组件]

---
+++ src/components/common/detail.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="wrapper" >
+      <section class="content">
+        
+      </section>
+  </section>
+</template>
+
+<script>
+export default {
+  name: '',
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style scoped lang='scss' >
+
+</style>
