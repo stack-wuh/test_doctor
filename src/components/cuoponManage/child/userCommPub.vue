2ca71c1d2b45233dbf3277b3505157f09de5c58b
wuhong <wuh131420@foxmail.com>
2018-09-11 18:05:20 +0800

    2018-09-11-diff[更新一下交互事件]

--- src/components/cuoponManage/child/userCommPub.vue
+++ src/components/cuoponManage/child/userCommPub.vue
@@ -3,51 +3,124 @@
       <section class="content">
         <Search />
         <section class="info">
-          <my-table :list="[{}]" header="true">
-            <span slot="title">佣金信息</span>
-          </my-table>
-        </section>
-        <!-- <section class="info">
-          <p class="nav-title c999">佣金提现</p>
+          <p class="nav-title c999">佣金信息</p>
           <el-form class="my-form" label-width="90px">
-              <el-form-item  label="提现金额" >
-                  <el-input class="my-input-320"></el-input>
+            <section class="inline-box">
+              <el-form-item label="车辆">
+                <el-input v-model="tempForm.carNumber" disabled></el-input>
               </el-form-item>
-              <el-form-item label="备注">
-                <el-input class="my-input-320"></el-input>
+              <el-form-item label="姓名">
+                <el-input v-model="tempForm.realName" disabled></el-input>
+              </el-form-item>
+              <el-form-item label="联系电话">
+                <el-input v-model="tempForm.phone" disabled></el-input>
+              </el-form-item>
+              <el-form-item label="会员等级">
+                <el-input v-model="tempForm.memberId" disabled></el-input>
+              </el-form-item>
+            </section>
+            <section class="inline-box">
+              <el-form-item label="佣金">
+                <el-input v-model="tempForm.brokerage" disabled></el-input>
+              </el-form-item>
+              <el-form-item label="余额">
+                <el-input v-model="tempForm.money" disabled></el-input>
+              </el-form-item>
+              <el-form-item label="会员积分">
+                <el-input v-model="tempForm.integral" disabled></el-input>
+              </el-form-item>
+            </section>
+          </el-form>
+        </section>
+        <section class="info">
+          <p class="nav-title c999">佣金提现</p>
+          <el-form class="my-form" :rules="rules" :model="form"  ref="myForm" label-width="90px">
+              <el-form-item  label="提现金额" prop="money" >
+                  <el-input class="my-input-320" v-model="form.money"></el-input>
+              </el-form-item>
+              <el-form-item label="备注" prop="remark">
+                <el-input class="my-input-320" v-model="form.remark" ></el-input>
               </el-form-item>
           </el-form>
-        </section> -->
-        <!-- <my-bottom type="button" /> -->
+        </section>
+        <my-sub-botton @handleCancel="cancel" @handleSubmit="submit"></my-sub-botton>
       </section>
   </section>
 </template>
 
 <script>
 import Search from '@/components/common/search'
-import MyBottom from '@/components/common/bottom'
 import MyTable from '@/components/common/myTable'
-import {mapActions, mapGetters} from 'vuex'
+import MySubBotton from '@/components/common/subButton'
+import {mapActions, mapGetters, mapState} from 'vuex'
+const rules = {
+  money:[
+    {
+      required:true,
+      message:'请编辑佣金提现的金额',
+      trigger:'blur'
+    },
+  ],
+  remark:[
+    {
+      required: false
+    }
+  ]
+}
 export default {
   name: 'memberPay',
   components:{
     Search ,
-    MyBottom ,
     MyTable ,
+    MySubBotton,
   },
   data () {
     return {
       inputDis:true,
+      form:{
+        money:'',
+        remark:'',
+      },
+      rules
     }
   },
-
-  methods: {
-    ...mapActions({
-      'getList':'getCouponStore'
+  computed:{
+    ...mapState({
+      'tempForm': state => state.Coupon.tempForm
     })
   },
+// 吴克 13294199207 鄂A 88888
+  methods: {
+    ...mapActions({
+      'getList':'getCouponStore',
+      'handleSubmit': 'couponMemberTakePut',
+      'handleCancel' : 'clearCouponMemberTake'
+    }),
+    cancel(){
+      this.$refs.myForm.resetFields()
+      this.handleCancel()
+    },
+    submit(){
+      this.$refs.myForm.validate(valid => {
+        if(valid){
+          this.form = {...this.form, id: this.tempForm.id}
+          this.handleSubmit({form: this.form}).then(res => {
+            this.cancel()
+            setTimeout(()=>{
+              this.$router.push({path:'/mid/container', query:{path: '/coupon/store', menu:'卡券管理', subMenu:'用户提现记录'}})
+            },1000)
+          })
+        }else{
+          _g.toastMsg({
+            type:'error',
+            msg:'请编辑必填项目后提交!'
+          })
+        }
+      })
+    }
+  },
   created(){
-    this.getList({path: this.$route.query.child || this.$route.query.subMenu})
+    // this.getList({path: this.$route.query.child || this.$route.query.subMenu})
   }
 }
 </script>
