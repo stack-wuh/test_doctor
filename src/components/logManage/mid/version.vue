cd0ae18473a0e50c8bbd01ec5a4be1afad646b9b
wuhong <wuh131420@foxmail.com>
2018-09-15 18:13:28 +0800

    2018-09-15-add[新增了版本更新的组件]

---
+++ src/components/logManage/mid/version.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="wrapper">
+    <p class="title">版本更新记录</p>
+    <section class="info">
+      <h4>2018年8月23日</h4>
+      <p>版本号: 18.8.23</p>
+      <p>新增: 进销存中商品项目可批量导入的功能</p>
+      <p>优化: 进销存中商品项目可批量导入的功能</p>
+      <p>修复: 进销存中商品项目可批量导入的功能</p>
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'version',
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style scoped lang='scss' >
+.wrapper{
+  padding-bottom: 20px;
+  background-color: #fff;
+  .title{
+    padding: 20px 0 25px 20px;
+  }
+  .info{
+    margin-bottom: 15px;
+    padding: 0 20px;
+  }
+}
+</style>
