376796be8ef3158afde0385d59a7aed7050e1ffe
wuhong <wuh131420@foxmail.com>
2018-09-18 15:01:12 +0800

    2018-09-18-update[系统消息详情页的交互]

--- src/components/logManage/sub/msgDetail.vue
+++ src/components/logManage/sub/msgDetail.vue
@@ -1,10 +1,13 @@
 <template>
   <section class="wrapper">
     <section class="info">
-      <header>
-        <h4>系统升级</h4>
-        <el-button>返回</el-button>
+      <header class="header">
+        <p>系统{{info.typeText}}</p>
+        <el-button @click="goBack" type="danger">返回</el-button>
       </header>
+      <time class="c666 f14" >{{info.publishTime}}</time><span class="margin-lf-15">{{info.organizationName}}</span>
+      <h4 class="margin-tp-15">{{info.title}}</h4>
+      <article class="article" v-html="info.content"></article>
     </section>
   </section>
 </template>
@@ -15,13 +18,31 @@ export default {
 
   data () {
     return {
+      info:{}
     }
   },
 
-  methods: {}
+  methods: {
+    goBack(){
+      this.$router.go(-2)
+    }
+  },
+  created(){
+    let data = this.$route.query.data && JSON.parse(this.$route.query.data)
+    this.info = data
+  }
 }
 </script>
 
 <style scoped lang='scss' >
-
+@import '../../../assets/style/mixin.scss';
+.wrapper{
+  header.header{
+    @include flex-box(row, nowrap, space-between, center);
+  }
+  .info{
+    padding:20px;
+    background-color: #fff;
+  }
+}
 </style>
