ef1f8553c036819db89a8277a1b1100374071dd1
wuhong <wuh131420@foxmail.com>
2018-09-18 20:31:52 +0800

    2018-09-18-update[新增了获取数据的钩子函数]

--- src/components/marketManage/mid/store.vue
+++ src/components/marketManage/mid/store.vue
@@ -1,13 +1,14 @@
 <template>
   <section class="father">
       <section class="content">
-        <Search />
-        <my-table v-if="isShow" header="true" :list="[{}]">
+        <Search @inputChange="getMarketStore({path: changePath})" :name="changePath" />
+        <my-table v-if="isShow" header="true" :list="formMarketStore({path: changePath})">
           <span slot="title">{{ $route.query.child || $route.query.subMenu}}列表</span>
           <div slot="right">
             <my-button></my-button>
           </div>
         </my-table>
+        <my-bottom @getCurrentPage="getCurrentPage" :total="total" :currentPage="currPageNo" />
       </section>
   </section>
 </template>
@@ -16,12 +17,16 @@
 import Search from '@/components/common/search'
 import MyTable from '@/components/common/myTable'
 import MyButton from '@/components/common/myButton'
+import MyBottom from '@/components/common/bottom'
+import {mapState, mapActions, mapGetters} from 'vuex'
+
 export default {
   name: 'store',
   components:{
     Search ,
     MyTable ,
-    MyButton ,
+    MyButton,
+    MyBottom,
   },
   data () {
     return {
@@ -29,19 +34,38 @@ export default {
     }
   },
   computed:{
+    ...mapState({
+      'total': state => state.Market.total,
+      'currPageNo': state => state.Market.currPageNo
+    }),
+    ...mapGetters({
+      'formMarketStore':'formMarketStore'
+    }),
     changePath(){
       return this.$route.query.child || this.$route.query.subMenu
     }
   },
   watch:{
     changePath(){
+      this.getMarketStore({path: this.changePath})
       this.isShow = false
       setTimeout(()=>{
         this.isShow = true
+        this.$store.commit('clearSearchForm')
       })
     }
   },
-  methods: {}
+  methods: {
+    ...mapActions({
+      'getMarketStore':'getMarketStore'
+    }),
+    getCurrentPage(val){
+      this.getMarketStore({path: this.changePath, currPageNo: val})
+    },
+  },
+  created(){
+    this.getMarketStore({path: this.changePath})
+  }
 }
 </script>
 
