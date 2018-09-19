08a7bf8863c00823c9efa6ae9f475b899a208bb5
wuhong <wuh131420@foxmail.com>
2018-09-19 15:38:24 +0800

    2018-09-19-update[会员充值接口报错了哦]

--- src/components/memberManage/child/memberPay.vue
+++ src/components/memberManage/child/memberPay.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="wrapper">
       <section class="content">
-        <Search />
+        <Search  />
         <section class="info">
           <p class="c999 nav-title">会员信息</p>
           <el-form class="my-form" label-width="90px" >
@@ -31,37 +31,32 @@
         </section>
         <section class="info">
           <p class="nav-title c999">会员充值</p>
-          <el-form class="my-form" :model="form">
+          <el-form class="my-form" :model="form" :rules="rules" ref="myForm">
             <section class="inline-box inline-other">
               <section class="box-item">
-                <el-form-item class="box-item" label="充值金额" label-width="90px">
-                  <el-input placeholder="请编辑充值金额" v-model="form.price"></el-input>
+                <el-form-item class="box-item" label="充值金额" label-width="90px" prop="money">
+                  <el-input placeholder="请编辑充值金额" v-model="form.money"></el-input>
                 </el-form-item>
-                <el-form-item class="box-item" label="赠送积分" label-width="90px" >
-                  <el-input placeholder="请编辑赠送积分" v-model="form.sendPoint"></el-input>
-                </el-form-item>
-              </section>
-              <section class="box-item">
-                <el-form-item label="赠送金额">
-                  <el-input placeholder="请编辑赠送金额" v-model="form.sendPrice"></el-input>
+                <el-form-item class="box-item" label="赠送积分" label-width="90px" prop="integral">
+                  <el-input placeholder="请编辑赠送积分" v-model="form.integral"></el-input>
                 </el-form-item>
               </section>
               <section class="box-item">
-                <el-form-item label="请选择赠送卡券">
-                  <el-select multiple placeholder="请选择赠送的卡券" v-model="form.coupon1">
+                <el-form-item label="赠送金额" prop="giveMoney">
+                  <el-input placeholder="请编辑赠送金额" v-model="form.giveMoney"></el-input>
+                </el-form-item>
+              </section>
+              <section class="box-item">
+                <el-form-item label="请选择赠送卡券" prop="ids">
+                  <el-select multiple placeholder="请选择赠送的卡券" v-model="form.ids">
                     <el-option v-for="(item,index) in couponList" :key="index"  :label="item.label" :value="item.value" ></el-option>
                   </el-select>
                 </el-form-item>
-                <!-- <el-form-item label="请选择赠送卡券">
-                  <el-select placeholder="请选择赠送的卡券" v-model="form.coupon2">
-                    <el-option label="bbb" value="111" ></el-option>
-                  </el-select>
-                </el-form-item> -->
               </section>
             </section>
           </el-form>
         </section>
-        <sub-button @handleSubmit="submit" />
+        <sub-button @handleCancel="cancel" @handleSubmit="submit" />
       </section>
   </section>
 </template>
@@ -70,6 +65,13 @@
 import Search from '@/components/common/search'
 import SubButton from '@/components/common/subButton'
 import {mapActions, mapGetters, mapState} from 'vuex'
+import {MaxCouponNumber} from '../../../utils/valid.js'
+const rules = {
+  money:[{required: true, message:'请编辑充值金额', trigger:'blur'}],
+  giveMoney:[{required: true, message:'请编辑赠送金额', trigger:'blur'}],
+  integral:[{required: true, message:'请编辑赠送积分', trigger:'blur'}],
+  ids:[{required: true, validator: MaxCouponNumber, trigger:'change'}],
+}
 
 export default {
   name: 'memberPay',
@@ -89,13 +91,18 @@ export default {
         member:'',
       },
       form:{
-
-      }
+        money:'',
+        giveMoney:'',
+        integral:'',
+        ids:[],
+      },
+      rules,
     }
   },
   computed:{
     ...mapState({
-      'tempObj': state => state.Member.tempObj.data
+      'tempObj': state => state.Member.tempObj.data,
+      'search': state => state.search
     }),
     ...mapGetters({
       'couponList': 'formatCouponList'
@@ -110,7 +117,31 @@ export default {
       'handleSubmit': 'memberRechargeDo'
     }),
     submit(){
-      this.handleSubmit({form: {name:111}})
+      let form = {...this.form, phone: this.search.phone}
+      if(!form.phone){
+        _g.toastMsg({
+          type:'error',
+          msg:'请选择编辑会员手机号之后提交'
+        })
+        return
+      }
+      this.$refs.myForm.validate(valid => {
+        if(valid){
+          this.handleSubmit({form})
+        }else{
+          _g.toastMsg({
+            type:'error',
+            msg: '请编辑必填项之后提交'
+          })
+        }
+      })
+    },
+    cancel(){
+      this.$refs.myForm.resetFields()
+      this.$store.commit('clearSearchForm')
+      setTimeout(()=>{
+        this.$router.push({path:'/member/list', query:{subMenu:'充值明细'}})
+      },1000)
     }
   },
   created(){
