bbd5159d6469edd559aea67722490ba077134e99
wuhong <wuh131420@foxmail.com>
2018-09-19 20:49:15 +0800

    2018-09-19-update[添加了一些处理钩子]

--- src/vuex/modules/market/store.js
+++ src/vuex/modules/market/store.js
@@ -15,11 +15,17 @@ const actions = {
     switch(path){
       case '普通活动' : _url = 'ordinaryActivities/ordinaryActivitiesList.do', search = {...rootState.search, currPageNo}, dispatch('getActiveList')
         break;
-      case '活动抽奖' : _url = 'activitiesDraws/activitiesDrawsList.do', search = {...rootState.search, currPageNo}
+      case '活动抽奖' : _url = 'activitiesDraws/activitiesDrawsList.do', search = {...rootState.search, currPageNo}, dispatch('getActiveList')
         break;
       case '推荐有礼' : _url = 'recommendingGift/recommendingGiftList.do', search = {...rootState.search, currPageNo}
         break;
       case '摇一摇活动' : _url = 'rockIngActivities/rockIngActivitiesList.do', search = {...rootState.search, currPageNo}
+        break;
+      case '精准获客' : _url = 'ordinaryActivities/accurateGuests.do', search = {...rootState.search, currPageNo}
+        break;
+      case '报名查询' : _url = 'ordinaryActivities/signUpList.do', search = {...rootState.search, currPageNo}, dispatch('getActiveList')
+        break;
+      case '活动中奖' : _url = 'activitiesDraws/userLotteryInfo.do', search = {...rootState.search, currPageNo}
         break;
     }
     return new Promise((resolve, reject) => {
@@ -29,6 +35,36 @@ const actions = {
       })
     })
 
+  },
+
+  /**
+   * 市场推广模块内容全部的删除事件
+   */
+  marketDelAndFresh({dispatch}, {row, row:{id}, path}){
+    let _url = ''
+    switch(path){
+      case '报名查询' : _url = 'ordinaryActivities/delSignUp.do'
+        break;
+      case '普通活动' : _url = 'ordinaryActivities/banActivities.do'
+        break;
+    }
+    return new Promise((resolve, reject) => {
+      $http.post(_url, {ids: id}, res => {
+        setTimeout(() => {
+          dispatch('getMarketStore', {path})
+        }, 1000);
+        return resolve(res)
+      })
+    })
+  },
+
+  /**
+   * 市场推广 -- 普通活动 -- 发布
+   */
+  marketActivePubAndFresh({dispatch}, {path, id} = {}){
+    $http.post('ordinaryActivities/releaseActivities.do', {id}, res => {
+      dispatch('getMarketStore', {path})
+    })
   }
 }
 
@@ -48,6 +84,8 @@ const getters = {
     return state.data.map(item => {
       if(path === '普通活动'){
         return {...item, statusText: item.status == 0 ? '已下架' : item.status == 1 ? '已发布' : '已推送', valid: `${item.startDateStr || ''}-${item.endDateStr || ''}`}
+      }else if(path === '报名查询'){
+        return {...item, isValidText: item.isValid == 0 ? '已失效' : '有效', statusText: item.status == 0 ? '未确认' : item.staus == 1 ? '已确认' : '已领取', isPlayText: item.isPlay == 0 ? '未支付' : '已支付'}
       }else{
         return {...item}
       }
