28ba7438a71d3acfca7d9d76d217a3911f0a670d
wuhong <wuh131420@foxmail.com>
2018-09-19 11:46:43 +0800

    2018-09-19-fixed[分出了一下查询事件]

--- src/vuex/search/click.js
+++ src/vuex/search/click.js
@@ -1,11 +1,8 @@
-export const  clearSearchForm = ({path} = {}) => {
+export const  clearSearchForm = ({path, menu} = {}) => {
   window.$store.commit('clearSearchForm')
   setTimeout(()=>{
-    switch(path){
-      case '用户佣金提现' : return window.$store.dispatch('couponMemberTake')
-      case '会员充值' : return window.$store.dispatch('memberRecharge')
-      case '保险公司' : return window.$store.dispatch('getStoresList', {path}) 
-      case '奖品卡券管理' : return window.$store.dispatch('getCouponStore', {path})
+    switch(menu){
+      case '卡券管理' : return window.$store.dispatch('getCouponStore', {path})
     }
   })
 }
@@ -14,11 +11,12 @@ export const searchFormList = ({path} = {}) => {
   window.$store.dispatch('getStoresList',{path})
 }
 
-export const handleSearchList = ({path} = {}) => {
-  switch(path){
-    case '用户佣金提现' : return window.$store.dispatch('couponMemberTake')
-    case '会员充值' : return window.$store.dispatch('memberRecharge')
-    case '保险公司' : return window.$store.dispatch('getStoresList', {path}) 
-    case '奖品卡券管理' : return window.$store.dispatch('getCouponStore', {path})
+export const handleSearchList = ({path, menu} = {}) => {
+  if(menu === '卡券管理'){
+    if(path === '用户佣金提现'){
+      return window.$store.dispatch('couponMemberTake')
+    }else{
+      return window.$store.dispatch('getCouponStore', {path })
+    }
   }
 }
