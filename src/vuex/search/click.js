4a7df7724101de50871781bd828edf05cd1dfd71
wuhong <wuh131420@foxmail.com>
2018-09-19 09:12:22 +0800

    2018-09-19-update[为重置按钮添加事件]

--- src/vuex/search/click.js
+++ src/vuex/search/click.js
@@ -1,7 +1,12 @@
 export const  clearSearchForm = ({path} = {}) => {
   window.$store.commit('clearSearchForm')
   setTimeout(()=>{
-    window.$store.dispatch('getStoresList',{path})
+    switch(path){
+      case '用户佣金提现' : return window.$store.dispatch('couponMemberTake')
+      case '会员充值' : return window.$store.dispatch('memberRecharge')
+      case '保险公司' : return window.$store.dispatch('getStoresList', {path}) 
+      case '奖品卡券管理' : return window.$store.dispatch('getCouponStore', {path})
+    }
   })
 }
 
@@ -14,5 +19,6 @@ export const handleSearchList = ({path} = {}) => {
     case '用户佣金提现' : return window.$store.dispatch('couponMemberTake')
     case '会员充值' : return window.$store.dispatch('memberRecharge')
     case '保险公司' : return window.$store.dispatch('getStoresList', {path}) 
+    case '奖品卡券管理' : return window.$store.dispatch('getCouponStore', {path})
   }
 }
