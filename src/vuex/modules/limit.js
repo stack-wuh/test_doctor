5d75bd2d573852da969dd32aa1d19c40317d105a
wuhong <wuh131420@foxmail.com>
2018-09-21 09:02:38 +0800

    2018-09-21-update[新增了获取权限的钩子和角色授权]

--- src/vuex/modules/limit.js
+++ src/vuex/modules/limit.js
@@ -9,18 +9,54 @@ const actions = {
    * 获取权限列表
    */
   getLimitStore({commit}, {form} = {}){
-    $http.post('authorityBackend/getAuthList.do', form, res => {
-      
-    })  
+    return new Promise((resolve, reject) => {
+      $http.post('authorityBackend/getAuthList.do', form, res => {
+        commit('setLimitStore', {params: res.data})
+        return resolve(res)
+      })  
+    })
+  },
+
+  /**
+   * 点击按钮, 角色授权
+   */
+  handleAccredit({dispatch}, {form} = {}){
+    return new Promise((resolve, reject) => {
+      $http.post('authorityBackend/authSet.do', form, res => {
+        setTimeout(()=>{
+          dispatch('getLimitStore')
+        },100)
+        return resolve(res)
+      })
+    })
   }
 }
 
 const mutations = {
-
+  setLimitStore(state, {params} = {}){
+    state.data = params && params.map(item => {
+      item.authorityMenuList.map(list => {
+        list.authorityList.map(sub => {
+          sub.isAuth = sub.isAuth === 1 ? true : false
+        })
+      })
+      return {id: item.id, menuName: item.menuName, subMenu: item.authorityMenuList}
+    })
+  }
 }
 
 const getters = {
-
+  formatLimitStore(state){
+    console.log(state ,' this is format')
+    return state.data.map(item => {
+      item.authorityMenuList.map(list => {
+        list.authorityList.map(sub => {
+          sub.isAuth = sub.isAuth === 1 ? true : false
+        })
+      })
+      return {id: item.id, menuName: item.menuName, subMenu: item.authorityMenuList}
+    })
+  }
 }
 
 export default {
