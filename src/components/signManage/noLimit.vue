f699adf5d096d7aa62626f88ab2cef39aa4bf95e
wuhong <wuh131420@foxmail.com>
2018-09-21 18:54:30 +0800

    2018-09-21-add[新增了无权访问的组件]

---
+++ src/components/signManage/noLimit.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="wrapper">
+    <section class="unlimit">
+      <section class="bg-img"></section>
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'nolimit',
+
+  data () {
+    return {
+      bgUrl: require('../../../src/assets/img/unlimit.png')
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style scoped lang='scss' >
+.wrapper{
+  width: 100%;
+  .bg-img{
+    width: 100%;
+    height: 200px;
+    background-image: url(../../../src/assets/img/unlimit.png);
+    background-position: center center;
+  }
+}
+</style>
