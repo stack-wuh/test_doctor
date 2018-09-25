21739d76049b7cbeeca469e4f322ff5346c0c46a
wuhong <wuh131420@foxmail.com>
2018-09-25 15:54:50 +0800

    2018-09-25-update[新增了添加新的表单]

--- src/components/marketManage/child/activePub.vue
+++ src/components/marketManage/child/activePub.vue
@@ -20,33 +20,43 @@
             <el-checkbox label="1" v-model="form.homePagePromotion">勾选时表示上传的照片会在首页轮播显示</el-checkbox>
           </el-form-item>
           <el-form-item label="报名费" prop="enrollFee">
-            <el-input v-model="form.enrollFee" placeholder="请编辑报名费"></el-input>
+            <el-input v-model="form.enrollFee" placeholder="请编辑报名费" class="my-input-220" ></el-input>
           </el-form-item>
           <el-form-item label="参与时强制支付" prop="partakeCompulsoryPayment" >
             <el-checkbox label="1" v-model="form.partakeCompulsoryPayment">勾选时表示参与时报名费用强制支付</el-checkbox>
           </el-form-item>
-          <el-form-item label="活动日期" prop="date" >
-            <el-date-picker v-model="form.startDate" value-format="yyyy-MM-dd"  type="daterange" range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期" ></el-date-picker>
+          <el-form-item label="活动日期" prop="date">
+            <el-date-picker v-model="form.date" value-format="yyyy-MM-dd"  type="daterange" range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期" ></el-date-picker>
           </el-form-item>
           <el-form-item label="参与活动奖励" prop="couponId">
             <el-select v-model="form.couponId" placeholder="请选择活动奖励">
-              <el-option label="aaa" value="aaa"></el-option>
+              <el-option v-for="(item, index) in couponList" :key="index" :label="item.label" :value="item.value"></el-option>
             </el-select>
           </el-form-item>
           <el-form-item label="初始报名数" prop="initialEnrollNum">
-            <el-select v-model="form.initialEnrollNum" placeholder="请选择初始报名数">
-              <el-option label="10" value="10"></el-option>
-            </el-select>
+            <el-input v-model="form.initialEnrollNum" placeholder="请编辑初始报名数" class="my-input-220" ></el-input>
           </el-form-item>
           <el-form-item label="是否允许报名" prop="whetherAllowEnroll" >
             <el-checkbox label="1" v-model="form.whetherAllowEnroll">勾选时表示允许报名</el-checkbox>
           </el-form-item>
           <el-form-item label="精准用户设置" >
             <section class="inline-box">
-              <el-select placeholder="浏览次数" v-model="form.viewCount" ></el-select>
-              <el-select placeholder="平均单词浏览最小时间" v-model="form.minTime"></el-select>
-              <el-select placeholder="平均单词浏览最大时间" v-model="form.maxTime"></el-select>
-              <el-checkbox label="勾选时表示两者都要满足" v-model="form.isok"></el-checkbox>
+              <el-form-item prop="viewCount">
+                <el-select placeholder="浏览次数" v-model.number="form.viewCount" clearable filterable allow-create default-first-option></el-select>
+              </el-form-item>
+              <el-form-item prop="minTime">
+                  <el-select placeholder="平均单词浏览最小时间" v-model="form.minTime">
+                    <el-option label="1分钟" value="1"></el-option>
+                  </el-select>
+              </el-form-item>
+              <el-form-item prop="maxTime">
+                <el-select placeholder="平均单词浏览最大时间" v-model="form.maxTime">
+                  <el-option label="1小时" value="60"></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item prop="isok">
+                <el-checkbox label="勾选时表示两者都要满足" v-model="form.conditionStatus"></el-checkbox>
+              </el-form-item>
             </section>
           </el-form-item>
         </el-form>
@@ -62,14 +72,22 @@ import {mapActions, mapState} from 'vuex'
 const rules = {
   activityTitle:[{required: true, message:'请编辑活动标题', trigger: 'blur'}],
   picture:[{required: true, message:'请上传活动封面', trigger: 'change'}],
-  homePagePromotion:[{required: true, message:'请勾选是否首页推广', trigger: 'change'}],
+  homePagePromotion:[{required: false, message:'请勾选是否首页推广', trigger: 'change'}],
   enrollFee:[{required: true, message:'请编辑报名费', trigger: 'blur'}],
-  partakeCompulsoryPayment:[{required: true, message:'请勾选报名费用是否强制支付', trigger: 'change'}],
+  partakeCompulsoryPayment:[{required: false, message:'请勾选报名费用是否强制支付', trigger: 'change'}],
   date:[{required: true, message:'请选择有效日期', trigger: 'change'}],
   couponId:[{required: true, message:'请选择奖励卡券', trigger: 'change'}],
-  initialEnrollNum:[{required: true, message:'请选择初始报名数', trigger: 'change'}],
-  whetherAllowEnroll:[{required: true, message:'请勾选是否允许报名', trigger: 'change'}],
+  initialEnrollNum:[{required: true, message:'请编辑初始报名数', trigger: 'change'}],
+  whetherAllowEnroll:[{required: false, message:'请勾选是否允许报名', trigger: 'change'}],
+  viewCount:[{required: true, message:'请选择浏览次数', trigger: 'change'},{type:'number', message:'请编辑内容为数字',trigger: 'change'}],
+  conditionStatus:[{required: false, message:'请勾选是否同时满足要求', trigger: 'change'}],
 }
+const minTime = [
+  {
+    label:'1分钟',
+    value:'1'
+  }
+]
 export default {
   name: 'activePub',
   components:{
@@ -91,22 +109,38 @@ export default {
         viewCount:'',
         minTime:'',
         maxTime:'',
-        date:'',
+        date:[],
       },
       rules,
     }
   },
   computed:{
     ...mapState({
-      'activesList':'activesList'
+      'activesList':'activesList',
+      'couponList': state => state.couponList
     })
   },
   methods: {
     ...mapActions({
-      'getActiveList':'getActiveList'
+      'getActiveList':'getActiveList',
+      'getCouponList':'getCouponList',
+      'handleSubmit': 'marketActivePutAndPost'
     }),
     submit(){
-      console.log(this.form)
+      this.$refs.myForm.validate(valid => {
+        if(valid){
+          this.handleSubmit({form: this.form}).then(res => {
+           setTimeout(()=>{
+              res.status == 0 && this.cancel()
+           },1000)  
+          })
+        }else{
+          _g.toastMsg({
+            type:'error',
+            msg:'请编辑必填项之后提交'
+          })
+        }
+      })
     },
     cancel(){
       this.$refs.myForm.resetFields()
@@ -118,6 +152,20 @@ export default {
   },
   created(){
     this.getActiveList()
+    this.getCouponList()
+    let data = this.$route.query.data && (JSON.parse(this.$route.query.data))
+    console.log(data)
+    if(data){
+      this.form = {
+        ...this.form,
+        ...data,
+        date:[data.startDate, data.endTime],
+        homePagePromotion: data.homePagePromotion === 1 ? true : false,
+        partakeCompulsoryPayment: data.partakeCompulsoryPayment === 1 ? true : false,
+        whetherAllowEnroll: data.whetherAllowEnroll === 1 ? true : false,
+        conditionStatus: data.conditionStatus === 1 ? true : false
+      }
+    }
   }
 }
 </script>
