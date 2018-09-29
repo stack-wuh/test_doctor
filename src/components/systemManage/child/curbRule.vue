b53c2c52b3ccbb2cf8a5bdeb31cea09b8eb020df
wuhong <wuh131420@foxmail.com>
2018-09-29 14:41:49 +0800

    2018-09-29-update[新增了表单内容]

--- src/components/systemManage/child/curbRule.vue
+++ src/components/systemManage/child/curbRule.vue
@@ -83,416 +83,447 @@
 </template>
 
 <script>
-import Bottom from '@/components/common/bottom'
-import { mapActions , mapState ,mapGetters} from 'vuex'
+import Bottom from "@/components/common/bottom";
+import { mapActions, mapState, mapGetters } from "vuex";
 /**
  * 组件需要参数 route.query.child
- * 会员卡积分规则发布 
+ * 会员卡积分规则发布
  * 关注后消息推送
  */
 export default {
-  name: 'curbRule',
-  components:{
-    Bottom,
+  name: "curbRule",
+  components: {
+    Bottom
   },
-  data () {
+  data() {
     return {
-      visibleDialog:false,
-      list:[
+      visibleDialog: false,
+      list: [
         {
-          price:'0-20',
-          intergal:1,
+          price: "0-20",
+          intergal: 1
         },
         {
-          price:'21-100',
-          intergal:9,
+          price: "21-100",
+          intergal: 9
         },
         {
-          price:'101-500',
-          intergal:29,
+          price: "101-500",
+          intergal: 29
         },
         {
-          price:'1001-2000',
-          intergal:79,
+          price: "1001-2000",
+          intergal: 79
         },
         {
-          price:'2001-5000',
-          intergal:599,
+          price: "2001-5000",
+          intergal: 599
         },
         {
-          price:'5001-',
-          intergal:899,
-        },
+          price: "5001-",
+          intergal: 899
+        }
       ],
-      form:[
+      form: [
         {
-          name:'会员卡积分规则',
-          list:[
+          name: "会员卡积分规则",
+          list: [
             {
-              label:'换算规则积分',
-              type:'input',
-              value:'',
-              prop:'ruleIntegra',
-              tips:'一个积分可抵扣金额(元)',
-              disabled: true,
+              label: "换算规则积分",
+              type: "input",
+              value: "",
+              prop: "ruleIntegra",
+              tips: "一个积分可抵扣金额(元)",
+              disabled: true
             },
             {
-              label:'首次关注',
-              type:'input',
-              value:'',
-              prop:'firstConcern',
-              tips:'永久一次',
-              disabled: true,
+              label: "首次关注",
+              type: "input",
+              value: "",
+              prop: "firstConcern",
+              tips: "永久一次",
+              disabled: true
             },
             {
-              label:'完善个人信息',
-              type:'input',
-              value:'',
-              prop:'improvePersonal',
-              tips:'永久一次',
-              disabled: true,
+              label: "完善个人信息",
+              type: "input",
+              value: "",
+              prop: "improvePersonal",
+              tips: "永久一次",
+              disabled: true
             },
             {
-              label:'完善车辆信息',
-              type:'input',
-              value:'',
-              prop:'improveVehicle',
-              tips:'永久一次',
-              disabled: true,
+              label: "完善车辆信息",
+              type: "input",
+              value: "",
+              prop: "improveVehicle",
+              tips: "永久一次",
+              disabled: true
             },
             {
-              label:'分享活动',
-              type:'input',
-              value:'',
-              prop:'sharingActivities',
-              tips:'每次',
-              disabled: true,
+              label: "分享活动",
+              type: "input",
+              value: "",
+              prop: "sharingActivities",
+              tips: "每次",
+              disabled: true
             },
             {
-              label:'登录/每日访问',
-              type:'input',
-              value:'',
-              prop:'dailyAccess',
-              tips:'每天三次',
-              disabled: true,
+              label: "登录/每日访问",
+              type: "input",
+              value: "",
+              prop: "dailyAccess",
+              tips: "每天三次",
+              disabled: true
             },
             {
-              label:'抽奖活动',
-              type:'input',
-              value:'',
-              prop:'luckyDraw',
-              tips:'每次',
-              disabled: true,
+              label: "抽奖活动",
+              type: "input",
+              value: "",
+              prop: "luckyDraw",
+              tips: "每次",
+              disabled: true
             },
             {
-              label:'推荐有礼',
-              type:'input',
-              value:'',
-              prop:'recommend',
-              tips:'每个活动每次',
-              disabled: true,
+              label: "推荐有礼",
+              type: "input",
+              value: "",
+              prop: "recommend",
+              tips: "每个活动每次",
+              disabled: true
             },
             {
-              label:'在线预约',
-              type:'input',
-              value:'',
-              prop:'appointment',
-              tips:'每天一次',
-              disabled: true,
+              label: "在线预约",
+              type: "input",
+              value: "",
+              prop: "appointment",
+              tips: "每天一次",
+              disabled: true
             },
             {
-              label:'在线预定',
-              type:'input',
-              value:'',
-              prop:'booking',
-              tips:'每天一次',
-              disabled: true,
+              label: "在线预定",
+              type: "input",
+              value: "",
+              prop: "booking",
+              tips: "每天一次",
+              disabled: true
             },
             {
-              label:'意见反馈',
-              type:'input',
-              value:'',
-              prop:'feedback',
-              tips:'采纳奖励',
-              disabled: true,
+              label: "意见反馈",
+              type: "input",
+              value: "",
+              prop: "feedback",
+              tips: "采纳奖励",
+              disabled: true
             },
             {
-              label:'问卷调查',
-              type:'input',
-              value:'',
-              prop:'survey',
-              tips:'每次',
-              disabled: true,
+              label: "问卷调查",
+              type: "input",
+              value: "",
+              prop: "survey",
+              tips: "每次",
+              disabled: true
             },
             {
-              label:'续保询价',
-              type:'input',
-              value:'',
-              prop:'renewInquiry',
-              tips:'每天一次',
-              disabled: true,
-            },
+              label: "续保询价",
+              type: "input",
+              value: "",
+              prop: "renewInquiry",
+              tips: "每天一次",
+              disabled: true
+            }
           ]
         },
         {
-          name:'关注后消息推送',
-          list:[
+          name: "关注后消息推送",
+          list: [
             {
-              label:'推送消息',
-              type:'select',
-              value:'',
-              prop:'pushType',
-              list:[
+              label: "推送消息",
+              type: "select",
+              value: "",
+              prop: "pushType",
+              list: [
                 {
-                  label:'文本',
-                  value:0
+                  label: "文本",
+                  value: 0
                 },
                 {
-                  label:'套餐',
-                  value:1
+                  label: "套餐",
+                  value: 1
                 },
                 {
-                  label:'活动',
-                  value:2
+                  label: "活动",
+                  value: 2
                 },
                 {
-                  label:'卡券',
-                  value:3
+                  label: "卡券",
+                  value: 3
                 }
               ],
-              change:this.handleChange
+              change: this.handleChange
             },
             {
-              label:'推送内容',
-              type:'textarea',
-              value:'',
-              list:[],
-              prop:'pushContent',
-              style:'width:320px',
+              label: "推送内容",
+              type: "textarea",
+              value: "",
+              list: [],
+              prop: "pushContent",
+              style: "width:320px"
             },
             {
-              label:'是否推送',
-              type:'radio',
-              value:'',
-              prop:'whetherPush',
-              list:[
+              label: "是否推送",
+              type: "radio",
+              value: "",
+              prop: "whetherPush",
+              list: [
                 {
-                  label:'推送',
-                  value:1
+                  label: "推送",
+                  value: 1
                 },
                 {
-                  label:'不推送',
-                  value:0
+                  label: "不推送",
+                  value: 0
                 }
               ]
             }
           ]
         }
       ],
-      integralRules:[
+      integralRules: [
         {
-          name:'抽奖活动',
-          value:'',
-          prop:'lotteryActivity',
+          name: "抽奖活动",
+          value: "",
+          prop: "lotteryActivity"
         },
         {
-          name:'新品试用',
-          value:'',
-          prop:'productTrial',
+          name: "新品试用",
+          value: "",
+          prop: "productTrial"
         }
-      ],
-    }
+      ]
+    };
   },
-  computed:{
+  computed: {
     ...mapState({
-      'data' : state => state.System.data ,
-      'interList':state => state.System.list1 ,
+      data: state => state.System.data,
+      interList: state => state.System.list1
     }),
     ...mapGetters({
-      'formatInterList' : 'formatInterForConsume',
-      'pushList':'formatPushList'
+      formatInterList: "formatInterForConsume",
+      pushList: "formatPushList"
     }),
-    formItem(){
-      return this.form.find(item => item.name === this.$route.query.child).list
+    formItem() {
+      return this.form.find(item => item.name === this.$route.query.child).list;
     },
-    formatFormItem(){
-      let keys =  this.data && Object.keys(this.data)
-       keys && keys.map(item => {
-        this.formItem.map(list => {
-          if(list.prop === item){
-            list.value = this.data[item]
-          }
-          list.id = this.data.id ? this.data.id : ''
-        })
-      })
-      return this.formItem
+    formatFormItem() {
+      let keys = this.data && Object.keys(this.data);
+      keys &&
+        keys.map(item => {
+          this.formItem.map(list => {
+            if (list.prop === item) {
+              list.value = this.data[item];
+            }
+            list.id = this.data.id ? this.data.id : "";
+          });
+        });
+      return this.formItem;
     },
+    changePath(){
+      return this.$route.query.child || this.$route.query.subMenu
+    }
   },
-  watch:{
-    pushList(){
-      this.$set(this.formatFormItem[1], 'list', this.pushList)
+  watch: {
+    pushList() {
+      this.$set(this.formatFormItem[1], "list", this.pushList);
+    },
+    changePath(){
+      if(this.changePath === '关注后消息推送'){
+        this.getPushInfo()
+      }else if(this.changePath === '会员卡积分规则'){
+       this.fetchDataForIntegral()
+      }
     }
   },
   methods: {
     ...mapActions({
-      'getInterList':'couponIntergalForConsume',
-      'getInterListRules':'getCouponIntegralRules',
-      'getPushList' : 'getPushList',
-      'couponPubAndPut':'couponPubAndPut',
-      'getCouponSaleRule':'getCouponSaleRule',
-      'couponMemberSalePub':'couponMemberSalePub',
-      'getCouponMemberSells':'getCouponMemberSells',
-      'setCouponMemberSells':'setCouponMemberSells'
+      getInterList: "couponIntergalForConsume",
+      getInterListRules: "getCouponIntegralRules",
+      getPushList: "getPushList",
+      couponPubAndPut: "couponPubAndPut",
+      getCouponSaleRule: "getCouponSaleRule",
+      couponMemberSalePub: "couponMemberSalePub",
+      getCouponMemberSells: "getCouponMemberSells",
+      setCouponMemberSells: "setCouponMemberSells",
+      getPushInfo: 'getPushInfo'
     }),
     /**
      * 消费返积分
      * 编辑与发布
      */
-    handleSubmitIntegralRule(){
+    handleSubmitIntegralRule() {
       let form = this.formatInterList.map(item => {
-        var range = item.range.split('-')
-        return {integral: item.integral, id: item.id, moneyMin: range[0], moneyMax: range[1]}
-      })
-      form = JSON.stringify(form)
-      this.$http.post('platform/addOrUpIntegralReturn.do', {data:form}, res => {
-        this.getInterList()
-      })
+        var range = item.range.split("-");
+        return {
+          integral: item.integral,
+          id: item.id,
+          moneyMin: range[0],
+          moneyMax: range[1]
+        };
+      });
+      form = JSON.stringify(form);
+      this.$http.post(
+        "platform/addOrUpIntegralReturn.do",
+        { data: form },
+        res => {
+          this.getInterList();
+        }
+      );
     },
-    handleChange(e){
-      this.$store.dispatch('getPushList', {pushType:e})
+    handleChange(e) {
+      this.$store.dispatch("getPushList", { pushType: e });
     },
 
     /**
      * 会员卡积分规则
      * 编辑/保存
      */
-    memberCouponRuleToggle(){
-      this.form[0].list.map(item => item.disabled = !item.disabled)
+    memberCouponRuleToggle() {
+      this.form[0].list.map(item => (item.disabled = !item.disabled));
     },
-    memberCouponCancel(){
-    },
+    memberCouponCancel() {},
     /**
      * 发布会员卡积分规则
      */
-    handleSubmitMemberRule(){
-      let form = {}
+    handleSubmitMemberRule() {
+      let form = {};
       this.form[0].list.forEach(item => {
-        form[item.prop] = item.value
-        form.id = item.id
-      })
-      this.couponPubAndPut({form}).then(res => {
-        this.memberCouponRuleToggle()
-        this.getInterListRules()
-      })
+        form[item.prop] = item.value;
+        form.id = item.id;
+      });
+      this.couponPubAndPut({ form }).then(res => {
+        this.memberCouponRuleToggle();
+        this.getInterListRules();
+      });
     },
     /**
      * 发布积分消费规则
      */
-    handleSubmitSaleRule(){
-      let form = {}
+    handleSubmitSaleRule() {
+      let form = {};
       this.integralRules.forEach(item => {
-        form[item.prop] = item.value
-        form.id = item.id
-      })
-      this.couponMemberSalePub({form}).then(res => {
-        res.status == 0 && this.getCouponSaleRule()
-      })
+        form[item.prop] = item.value;
+        form.id = item.id;
+      });
+      this.couponMemberSalePub({ form }).then(res => {
+        res.status == 0 && this.getCouponSaleRule();
+      });
     },
 
     /**
      * 发布积分抵现
      */
-    handleSubmitToCash(){
-      let ids = [], tocashs = []
+    handleSubmitToCash() {
+      let ids = [],
+        tocashs = [];
       this.list.map(item => {
-        if(item.tocash){
-          ids = [...ids, item.id]
-          tocashs = [...tocashs, item.tocash]
+        if (item.tocash) {
+          ids = [...ids, item.id];
+          tocashs = [...tocashs, item.tocash];
         }
-      })
-      this.setCouponMemberSells({form:{ids: ids.toString(), tocashs: tocashs.toString()}})
+      });
+      this.setCouponMemberSells({
+        form: { ids: ids.toString(), tocashs: tocashs.toString() }
+      });
+    },
+
+    fetchDataForIntegral(){
+      this.getInterList();
+      this.getInterListRules();
+      this.getPushList();
+      this.getCouponSaleRule().then(res => {
+        let keys = res.data && Object.keys(res.data);
+        keys.forEach(item => {
+          this.integralRules.map(list => {
+            if (list.prop === item) {
+              list.value = res.data[item];
+              list.id = res.data.id;
+            }
+          });
+        });
+      });
+      this.getCouponMemberSells().then(res => {
+        this.list = res.data;
+      });
     }
-  } , 
-  created(){
-    this.getInterList()
-    this.getInterListRules()
-    this.getPushList()
-    this.getCouponSaleRule().then(res => {
-      let keys = res.data && Object.keys(res.data)
-      keys.forEach(item => {
-        this.integralRules.map(list => {
-          if(list.prop === item){
-            list.value = res.data[item]
-            list.id = res.data.id
-          }
-        })
-      })
-    })
-    this.getCouponMemberSells().then(res => {
-      this.list = res.data
-    })
   },
-}
+  created() {
+    if(this.changePath === '关注后消息推送'){
+      this.getPushInfo()
+    }else if(this.changePath === '会员卡积分规则'){
+      this.fetchDataForIntegral()
+    }
+  }
+};
 </script>
 
 <style scoped lang='scss' >
-@import '../../../assets/style/mixin.scss';
-.wrapper{
+@import "../../../assets/style/mixin.scss";
+.wrapper {
   height: calc(100% - 20px);
-  .content{
-    padding-bottom:15px;
+  .content {
+    padding-bottom: 15px;
     background-color: #fff;
-    section.form{
-      margin:15px 0;
-      .el-form{
+    section.form {
+      margin: 15px 0;
+      .el-form {
         display: flex;
         flex-flow: row wrap;
-        .el-form-item{
-          .el-input{
-            margin-right:15px;
-            +span{
+        .el-form-item {
+          .el-input {
+            margin-right: 15px;
+            + span {
               color: #999;
             }
           }
         }
       }
     }
-    section.inter-box{
-      p{
-        margin-bottom:15px;
+    section.inter-box {
+      p {
+        margin-bottom: 15px;
       }
-      p.t-title{
+      p.t-title {
         margin-left: 15px;
         color: #999;
       }
-      .my-table{
+      .my-table {
         width: 95%;
-        margin:0 auto;
+        margin: 0 auto;
       }
     }
-    section.block-box{
-      width:100%;
+    section.block-box {
+      width: 100%;
     }
-    section.btn-area{
-      margin:15px 0;
+    section.btn-area {
+      margin: 15px 0;
       text-align: center;
     }
-    section.inline-box{
+    section.inline-box {
       width: 50%;
     }
-    .flex-box{
+    .flex-box {
       display: flex;
       flex-flow: row nowrap;
       align-items: center;
       justify-content: space-between;
-      margin:15px 0;
-      .el-button{
+      margin: 15px 0;
+      .el-button {
         margin-right: 3%;
       }
     }
   }
-  .btn-area{
-    padding-bottom:10px;
+  .btn-area {
+    padding-bottom: 10px;
   }
 }
 </style>
