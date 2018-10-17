67180a0f14a5d448f595ba53c915e8aa98745c29
wuhong <wuh131420@foxmail.com>
2018-10-17 16:32:22 +0800

    2018-10-17-[add][新增了添加编辑专项检测配置的页面]

---
+++ src/components/serveManage/child/settingOnePub.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="setting-wrapper">
+     
+  </section>
+</template>
+
+<script>
+export default {
+  name: '',
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style scoped lang='scss' >
+
+</style>
