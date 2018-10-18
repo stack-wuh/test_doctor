cf8a6d9388758f346a9bf6162424742ec9e33fbd
wuhong <wuh131420@foxmail.com>
2018-10-18 14:39:02 +0800

    2018-10-18-[add][新增了两个数据可视化组件]

---
+++ src/components/common/charts/barChart.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="bar-wrapper">
+    <section :class="'barChart' + index"></section>
+  </section>
+</template>
+
+<script>
+const option = {
+  xAxis:{
+    type:'category',
+    data:['1','2','3','4','5','6','7']
+  },
+  yAxis:{
+    type:'value'
+  },
+  series:[{
+    data:[100,100,100,11,22],
+    type:'line',
+    smooth: true
+  }]
+}
+
+export default {
+  name: '',
+  props:{
+    index:{
+      type:Number,
+      default:1
+    }
+  },
+  data () {
+    return {
+    }
+  },
+
+  methods: {},
+  created(){
+    this.$nextTick(()=> {
+      let chart = echarts.init(document.querySelector('barChart' + this.index))
+      chart.setOption(option)
+    })
+  }
+}
+</script>
+
+<style scoped lang='scss' >
+
+</style>
