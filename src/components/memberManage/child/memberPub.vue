857d49e67797b4553dbb684dc591cecdb8962bed
wuhong <wuh131420@foxmail.com>
2018-10-18 14:38:41 +0800

    2018-10-18-[fixed][修复了一下跳转详情页性别栏为1]

--- src/components/memberManage/child/memberPub.vue
+++ src/components/memberManage/child/memberPub.vue
@@ -13,8 +13,8 @@
         <section class="inline-box">
           <el-form-item label="性别" prop="sex" >
             <el-select placeholder="请选择性别" v-model="form.sex">
-              <el-option label="男" value="1"></el-option>
-              <el-option label="女" value="0"></el-option>
+              <el-option label="男" :value="1"></el-option>
+              <el-option label="女" :value="0"></el-option>
             </el-select>
           </el-form-item>
           <el-form-item label="客户生日"  prop="birthday" >
