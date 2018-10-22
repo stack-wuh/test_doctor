358cae9b765cb986d06cc7f79ef0db945b0ff2f0
wuhong <wuh131420@foxmail.com>
2018-10-22 14:54:24 +0800

    2018-10-22-[update][更新了图表组件的数据类型,和设置]

--- src/components/common/charts/barChart.vue
+++ src/components/common/charts/barChart.vue
@@ -1,24 +1,63 @@
 <template>
   <section class="bar-wrapper">
-    <section :class="'barChart' + index"></section>
+    <section class="barChart" :class="'barChart' + index"></section>
   </section>
 </template>
 
 <script>
 const option = {
-  xAxis:{
-    type:'category',
-    data:['1','2','3','4','5','6','7']
-  },
-  yAxis:{
-    type:'value'
-  },
-  series:[{
-    data:[100,100,100,11,22],
-    type:'line',
-    smooth: true
-  }]
-}
+    color: ['#3398DB'],
+    title:{
+      text:'图标标题',
+      top:'20',
+      left:'20',
+    },
+    toolbox: {
+        show: true,
+        feature: {
+            saveAsImage: {}
+        },
+        right:40,
+        top:20,
+    },
+    tooltip : {
+        trigger: 'axis',
+        axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+            type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+        }
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    xAxis : [
+        {
+            type : 'category',
+            data : ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            axisTick: {
+                alignWithLabel: true
+            }
+        }
+    ],
+    yAxis : [
+        {
+            type : 'value',
+            minInterval: 1
+        }
+    ],
+    series : [
+        {
+            name:'直接访问',
+            type:'bar',
+            barWidth: '60%',
+            data:[10, 52, 200, 334, 390, 330, 220]
+        }
+    ],
+    
+};
+
 
 export default {
   name: '',
@@ -30,19 +69,35 @@ export default {
   },
   data () {
     return {
+      option,
     }
   },
 
   methods: {},
   created(){
     this.$nextTick(()=> {
-      let chart = echarts.init(document.querySelector('barChart' + this.index))
-      chart.setOption(option)
+      let els = document.querySelectorAll('.barChart' + this.index)
+      let chart = echarts.init(els[0])
+      chart.setOption(this.option)      
     })
   }
 }
 </script>
 
 <style scoped lang='scss' >
-
+.bar-wrapper{
+  width: 100%;
+  height: 100%;
+  min-height: 400px;
+  position: relative;
+  margin-top:20px;
+  background-color: #fff;
+  .barChart{
+    position: absolute;
+    top:0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+  }
+}
 </style>
---
+++ src/components/common/charts/lineChart.vue
@@ -0,0 +1,80 @@
+<template>
+  <section class="line-wrapper">
+    <section class="lineChart" :class="'lineChart' + index"></section>
+  </section>
+</template>
+
+<script>
+const option = {
+    title:{
+      text:'图标标题',
+      top:'20',
+      left:'20',
+    },
+    toolbox: {
+        show: true,
+        feature: {
+            saveAsImage: {}
+        },
+        right:40,
+        top:20,
+    },
+    xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+        type: 'value',
+        minInterval: 1,
+    },
+    series: [{
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line',
+        smooth: true
+    }]
+};
+
+
+
+export default {
+  name: '',
+  props:{
+    index:{
+      type:Number,
+      default:1
+    }
+  },
+  data () {
+    return {
+      option,
+    }
+  },
+
+  methods: {},
+  created(){
+    this.$nextTick(()=> {
+      let els = document.querySelectorAll('.lineChart' + this.index)
+      let chart = echarts.init(els[0])
+      chart.setOption(this.option)      
+    })
+  }
+}
+</script>
+
+<style scoped lang='scss' >
+.line-wrapper{
+  width: 100%;
+  height: 100%;
+  min-height: 400px;
+  position: relative;
+  margin-top:20px;
+  background-color: #fff;
+  .lineChart{
+    position: absolute;
+    top:0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+  }
+}
+</style>
